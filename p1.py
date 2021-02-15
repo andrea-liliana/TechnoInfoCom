4dbcf1e0196d4224dcbc3b802c757ff9c73b8b87
Stéphane Champailler <schampailler@skynet.be>
2021-02-15 20:02:58 +0100

    added conditional entropy

--- p1.py
+++ p1.py
@@ -14,19 +14,25 @@ def joint_entropy(jp):
 
 
 def conditional_entropy(X_given_Y, Y):
-    """ cp : conditional probability P(X|Y)
-    mp: marginal probability P(Y)
+    """
+    X_given_Y : conditional probability P(X|Y)
+                X_i | Y_i : row(i), column(j)
+    Y: marginal probability P(Y)
 
     Recall P(X,Y) = P(X|Y) P(Y)
 
     => P(X_i, Y_j) = P(X_i|Y_j) * P(Y_j)
     """
 
+    # Compare nulber of columns in X_given_Y (that's number of Y's)
+    # to length of the Y vector.
+    assert X_given_Y.shape[1] == Y.shape[1], f"Shape don't match : {X_given_Y.shape[1]} != {Y.shape[1]}"
+
     # FIXME not tested yet !!!
 
     z = np.tile(Y, (X_given_Y.shape[0], 1))
 
-    return - X_given_Y * z * np.log2(X_given_Y)
+    return - np.sum(X_given_Y * z * np.log2(X_given_Y))
 
 
 
@@ -38,5 +44,8 @@ if __name__ == "__main__":
     assert entropy(p) == 2, "4 outcomes needs 2 bits"
 
     jp = np.array([[0.25, 0.25], [0.25, 0.25]])
-    print(jp)
     assert joint_entropy(jp.flatten()) == 2, f"{joint_entropy(jp)}"
+
+    X_given_Y = np.array([[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]])
+    Y = np.array([[0.1, 0.2, 0.7]])
+    print(conditional_entropy(X_given_Y, Y))
