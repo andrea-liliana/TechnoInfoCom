65ebac5926aa3b73e5573058544d5fed515d0ec3
Stéphane Champailler <schampailler@skynet.bewq>
2021-02-20 15:08:39 +0100

    added proofs

--- p1.py
+++ p1.py
@@ -17,7 +17,9 @@ def joint_entropy(X_and_Y):
     Compute H(X ^ Y)
 
     X_and_Y : joint probabilities, a 2D numpy array such that
-    X_and_Y[i,j] == P[X_i ^ Y_j]
+              X_and_Y[i,j] == P[X_i ∩ Y_j]
+
+    We don't assume X is conditionally independent of Y.
     """
     return entropy(X_and_Y)
 
@@ -30,15 +32,16 @@ def conditional_entropy(X_given_Y, Y):
                 X_i | Y_i : row(i), column(j)
     Y: marginal probability P(Y)
 
-    Recall P(X,Y) = P(X|Y) P(Y)
-
-    => P(X_i, Y_j) = P(X_i|Y_j) * P(Y_j)
+    Slide 3, course 2 : H(X |Y) = − Σ Σ P(Xi ∩ Yj) log P(Xi | Yj)
+    Recall that P(X ∩ Y) = P(X | Y) P(Y) => We can use the marginal
+    => P(X_i ∩ Y_j) = P(X_i|Y_j) * P(Y_j)
     """
 
-    # Compare nulber of columns in X_given_Y (that's number of Y's)
+    # Compare number of columns in X_given_Y (that's number of Y's)
     # to length of the Y vector.
     assert X_given_Y.shape[1] == Y.shape[1], f"Shape don't match : {X_given_Y.shape[1]} != {Y.shape[1]}"
 
+    # z helps to compute combinations of i and j indices.
     z = np.tile(Y, (X_given_Y.shape[0], 1))
 
     return - np.sum(X_given_Y * z * np.log2(X_given_Y))
@@ -62,10 +65,21 @@ def mutual_information(X, Y, X_and_Y):
 
 
 def joint_entropy2(X,Y,Z):
+    """
+    FIXME
+    I'm afraid this suggests P(X,Y,Z) = P(X)P(Y)P(Z) which is true
+    only if X ⊥ Y ⊥ Z. No ?
+    """
+
     probability = []
     for x1 in set(X):
         for x2 in set(Y):
             for x3 in set(Z):
+
+                # FIXME The doc : https://numpy.org/doc/stable/reference/generated/numpy.logical_and.html
+                # says something different...
+
+                # FIXME I don't understand the np.mean.
                 probability.append(np.mean(np.logical_and(X == x1, Y == x2, Z==x3)))
 
     return np.sum(-p * np.log2(p) for p in probability)
@@ -73,16 +87,37 @@ def joint_entropy2(X,Y,Z):
 def cond_mutual_information(X_and_Z, Y_and_Z, X, Y, Z):
     """
     I(X,Y|Z) = H(X,Z)+ H(Y,Z) - H(Z) - H(X,Y,Z)
-    H(X,Y,Z) = H(X,Y|Z)+H(Z)
-    H(X,Y|Z) = H(X|Z) + H(Y|X,Z)
+
+    H(X,Y,Z) = H(X,Y|Z)+H(Z) => Stef: H(X,Y,Z) = H(X | Y,Z) + H(Y | Z) + H(Z)  (chain rule)
+                                               = H(Y,X|Z) - H(Y|Z) + H(Y | Z) + H(Z) (see rule below)
+                                               = H(Y,X|Z) + H(Z)
+
+    H(X,Y|Z) = H(X|Z) + H(Y|X,Z) => from the course (Slide 9, point 2)
+
+    which we prove like this :
+
+    H(X,Y|Z) = - Σ_X Σ_Y Σ_Z P(X,Y ∩ Z) log(P(X,Y|Z))
+             = - Σ_X Σ_Y Σ_Z P(X,Y,Z) log(P(Y|Z,X) P(X|Z))  # Because P(X,Y|Z) = P(X,Y,Z)/P(Z)
+                                                                               = (P(X,Y,Z)/P(X,Z)))*(P(X,Z)/P(Z)
+                                                                               = P(Y|X,Z) * P(X|Z)
+             = - Σ_X Σ_Y Σ_Z P(X,Y,Z) log P(Y|Z,X)  # because log of product
+               - Σ_X Σ_Y Σ_Z P(X,Y,Z) log P(X|Z)
+             = H(Y|Z,X)  # because feinition of H
+               - Σ_Y P(Y|Z,X) Σ_X Σ_Z P(Z,X) log P(X|Z)
+             = H(Y|Z,X)
+               + 1 * H(Z,X)  # Because Y probability distribution => Σ_i Y_i = 1
+             = H(Y|Z,X) + H(X,Z)
     """
     return joint_entropy(X_and_Z) + joint_entropy(Y_and_Z) - entropy(Z) - joint_entropy2(X,Y,Z)
 
 def cond_joint_entropy(X,Y,Z):
     """
+    Computes H(X,Y|Z)
+
     H(X,Y|Z) this term is obtained here: H(X,Y,Z) = H(X,Y|Z)+H(Z)
-    H(X,Y|Z) = H(X,Y,Z) - H(Z)
+    H(X,Y|Z) = H(X,Y,Z) - H(Z) (see above)
     """
+
     return joint_entropy2(X,Y,Z) - entropy(Z)
 
 
