a1f8d662cde9f16871347bf21d90615cc1c9767c
Stéphane Champailler <schampailler@skynet.bewq>
2021-02-22 17:31:13 +0100

    fixes

--- p1_q2.py
+++ p1_q2.py
@@ -5,15 +5,30 @@ if __name__ == "__main__":
     df = pd.read_csv('P1_medicalDB.csv')
     print(df)
 
+    # I build a table with same structure as the original
+    # data frame but where each value is replaced by the probability
+    # of that value occuring.
+
     proba = df.copy()
 
+    # One column at a time
     for cname in df:
+        # Count each value appear in the column and
+        # divide to get its proportion, that is,
+        # its probabiliyt
 
-        #print(df[cname].value_counts())
         a = df[cname].value_counts() / len(df)
-        #print(a.to_dict())
+
+        # Replace values by probabilities
         proba[cname] = proba[cname].map(
             a.to_dict())
 
-    proba['joint'] = proba.product(1)
-    print(proba)
+    # Compute the joint probability on each *row*
+    # by multiplyin all rows together.
+    proba['joint'] = proba.product(axis=1)
+
+    # Clean values so we have a proabability distribution
+    # (ie all probabilities sum to one)
+    proba['joint'] = proba['joint'] / proba['joint'].sum()
+
+    print(proba['joint'].sum())
