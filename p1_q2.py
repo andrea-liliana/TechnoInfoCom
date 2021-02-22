ee46fe9af7e51c7b9974c92615c4490f2c427219
Stéphane Champailler <schampailler@skynet.bewq>
2021-02-22 17:25:39 +0100

    prototype for computing joint probabilities

--- p1_q2.py
+++ p1_q2.py
@@ -1,6 +1,19 @@
-from pandas import DataFrame, read_csv
+import pandas as pd
 
 if __name__ == "__main__":
 
-    df = read_csv('P1_medicalDB.csv')
+    df = pd.read_csv('P1_medicalDB.csv')
     print(df)
+
+    proba = df.copy()
+
+    for cname in df:
+
+        #print(df[cname].value_counts())
+        a = df[cname].value_counts() / len(df)
+        #print(a.to_dict())
+        proba[cname] = proba[cname].map(
+            a.to_dict())
+
+    proba['joint'] = proba.product(1)
+    print(proba)
