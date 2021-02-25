cc0334bcb51069c6922f2e090f3d5504683fcafd
Stéphane Champailler <schampailler@skynet.bewq>
2021-02-25 08:50:58 +0100

    question 6 v0

--- p1_q2.py
+++ p1_q2.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         # Count how many times each value appears in the column and
         # divide to get its proportion, that is, its probability
 
-        a = df[cname].value_counts() / len(df)
+        a = df[cname].value_counts(normalize=True)
 
         # Replace values by probabilities
         proba[cname] = proba[cname].map(
@@ -32,3 +33,14 @@ if __name__ == "__main__":
     proba['joint'] = proba['joint'] / proba['joint'].sum()
 
     print(proba['joint'].sum())
+
+    # Question 6 -----------------------------------------------------
+
+    """ Entropy of a variable X is - sum p_i log p_i where p_i is the
+    probability of the variable to have value X_i """
+
+    for cname in df:
+        vc = df[cname].value_counts(normalize=True)
+        a = np.array(vc.values)
+        entropy = -np.sum(a * np.log2(a))
+        print(f"{cname}\t{vc.values}\t{entropy:.3f}")
