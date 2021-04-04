3157a8281b4971674a68ac89983374245dff4de8
Stéphane Champailler <schampailler@skynet.bewq>
2021-04-04 14:12:24 +0200

    working now

--- p2_lz.py
+++ p2_lz.py
@@ -1,3 +1,4 @@
+import numpy as np
 import math
 
 message = "TGTGTAAACCTTGGTATTGGAATGTAAACACCTTGACACAGAGGTTAGCATTAACACTTAAATGATTAGTTTTTGATCAGTCTATAGATATGGTAGCGTGGAGAGTTTGTGACGGATCCGTGTGGTGAGTGAACAACTACAACTTAGTGTCCGGGAATTCCGGAATCACAGTGTTCGACAGAATACGCGTGGACCGTGGTCAGGAGTATCACAGTGGCGACAAGGACGGGATCTTGAATTGGTAAGAAAATGAACAGTTAGTTGATTCGAATTCATTTGTTTACCTACGTTGTGTACCGG"
@@ -7,41 +8,53 @@ coded_bin = ""
 
 prefix = ""
 prefixes = {"" : 0}
-for c in message:
+for i, c in enumerate(message):
 
-    if prefix + c in prefixes:
+    if i < len(message) - 1 and prefix + c in prefixes:
         prefix = prefix + c
     else:
         coded_message.append( [prefixes[prefix], c] )
 
-        coded_bin += f"X{prefixes[prefix]:b}{ord(c):8b}"
+        l = math.ceil(math.log2(len(prefixes)))
+        if l == 0:
+            l = 1
+
+        coded_bin += f"{np.binary_repr(prefixes[prefix],l)}{ord(c):08b}"
 
         prefixes[prefix + c] = len(prefixes)
 
         prefix = ""
 
+# if prefix != "":
+#     l = math.ceil(math.log2(len(prefixes)))
+#     if l == 0:
+#         l = 1
+
+#     coded_bin += f"{np.binary_repr(prefixes[prefix],l)}{ord(c):08b}"
+
 
 print(len(message))
 print(len(coded_message))
-print(coded_bin)
+# print(coded_bin)
 
 ndx = 0
 decoded = ""
 prefixes = {0: ""}
-while True:
+while ndx < len(coded_bin):
     l = math.ceil(math.log2(len(prefixes)))
     if l == 0:
         l = 1
     prefix_code = int(coded_bin[ndx:ndx+l], 2)
-    print(f"ndx:{ndx}, l:{l}, len pfx:{len(prefixes)}, pfx code:{prefix_code}")
+
+    #print(f"ndx:{ndx}, l:{l}, len pfx:{len(prefixes)}, pfx code:{prefix_code}")
     c = chr(int(coded_bin[ndx+l:ndx+l+8], 2))
+    # c = coded_bin[ndx+l:ndx+l+1]
+
     decoded += prefixes[prefix_code] + c
-    print(l,prefix_code,c)
+    #print(l,prefix_code,c,decoded)
 
     prefixes[len(prefixes)] = prefixes[prefix_code] + c
     ndx = ndx+l+8
-
-
 
 # decoded = ""
 # prefixes = {0: ""}
@@ -51,4 +64,4 @@ while True:
 
 print(decoded)
 
-assert decoded == message[:-1]
+assert decoded == message
