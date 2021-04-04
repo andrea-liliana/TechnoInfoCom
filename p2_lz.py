8b8013e3a0715b3e22c480deae35680c94a5bdb0
Stéphane Champailler <schampailler@skynet.bewq>
2021-04-04 15:03:30 +0200

    improvements

--- p2_lz.py
+++ p2_lz.py
@@ -1,40 +1,57 @@
+import os
 import numpy as np
 import math
 
-message = "TGTGTAAACCTTGGTATTGGAATGTAAACACCTTGACACAGAGGTTAGCATTAACACTTAAATGATTAGTTTTTGATCAGTCTATAGATATGGTAGCGTGGAGAGTTTGTGACGGATCCGTGTGGTGAGTGAACAACTACAACTTAGTGTCCGGGAATTCCGGAATCACAGTGTTCGACAGAATACGCGTGGACCGTGGTCAGGAGTATCACAGTGGCGACAAGGACGGGATCTTGAATTGGTAAGAAAATGAACAGTTAGTTGATTCGAATTCATTTGTTTACCTACGTTGTGTACCGG"
+INPUT_FILE = "genome.txt"
+
 
 coded_message = []
 coded_bin = ""
 
 prefix = ""
 prefixes = {"" : 0}
-for i, c in enumerate(message):
-
-    if i < len(message) - 1 and prefix + c in prefixes:
-        prefix = prefix + c
-    else:
-        coded_message.append( [prefixes[prefix], c] )
-
-        l = math.ceil(math.log2(len(prefixes)))
-        if l == 0:
-            l = 1
-
-        coded_bin += f"{np.binary_repr(prefixes[prefix],l)}{ord(c):08b}"
-
-        prefixes[prefix + c] = len(prefixes)
-
-        prefix = ""
-
-# if prefix != "":
-#     l = math.ceil(math.log2(len(prefixes)))
-#     if l == 0:
-#         l = 1
-
-#     coded_bin += f"{np.binary_repr(prefixes[prefix],l)}{ord(c):08b}"
 
 
-print(len(message))
-print(len(coded_message))
+with open(INPUT_FILE, 'r') as f:
+
+    # The whole point of this reickery is to detect
+    # EOF one char ahead. This way we can tell when
+    # we're processing the last character of the
+    # input file
+
+    c = f.read(1)
+    eof = c == ''
+    next_char = f.read(1)
+
+    while True:
+        if not eof and prefix + c in prefixes:
+            prefix = prefix + c
+        else:
+            # coded_message.append( [prefixes[prefix], c] )
+
+            # FIXME one must optimize this
+            l = math.ceil(math.log2(len(prefixes)))
+            if l == 0:
+                l = 1
+
+            coded_bin += f"{np.binary_repr(prefixes[prefix],l)}{ord(c):08b}"
+
+            prefixes[prefix + c] = len(prefixes)
+            prefix = ""
+
+        if next_char != '':
+            c = next_char
+            next_char = f.read(1)
+            eof = next_char == ''
+        else:
+            break
+
+print(os.path.getsize(INPUT_FILE))
+
+# Test : from 0/1 string to np array
+print(np.frombuffer(np.array(map(int, coded_bin)), np.uint8))
+
+print(len(coded_bin) // 8)
 # print(coded_bin)
 
 ndx = 0
@@ -62,6 +79,7 @@ while ndx < len(coded_bin):
 #     prefixes[len(prefixes)] = prefixes[prefix_code] + c
 #     decoded += prefixes[prefix_code] + c
 
-print(decoded)
+#print(decoded)
 
-assert decoded == message
+with open(INPUT_FILE, 'r') as f:
+    assert decoded == f.read()
