f8e9d9a7a30ce74a10f870c8bdb494e5779e5529
Stéphane Champailler <schampailler@skynet.bewq>
2021-04-15 17:25:39 +0200

    finished hamming code implementation

--- hamming.py
+++ hamming.py
@@ -2,54 +2,107 @@ import numpy as np
 
 
 def bin_array(num, m):
-    """Convert a positive integer num into an m-bit bit vector"""
+    """Convert a positive integer num into a numpy m 0/1 elements vector"""
     return np.array(list(np.binary_repr(num).zfill(m))).astype(np.int8)
 
 
-DATA_BITS, CODE_BITS = 4, 7
-PARITY_BITS = CODE_BITS - DATA_BITS
+class HammingCode:
+    def __init__(self, data_bits, code_bits):
+        self.data_bits = data_bits
+        self.code_bits = code_bits
+        self.parity_bits = code_bits - data_bits
+
+        assert code_bits > data_bits
+        self._precompute()
+
+    def encode(self, data):
+        assert len(data) == self.data_bits
+        return (data @ self.G) % 2
+
+    def decode(self, code):
+        parity_check = (self.H @ code) % 2
+        correction = self.correction_map[np.dot(self.POWER2, parity_check)]
+        corrected_code = (code + correction) % 2
+        return corrected_code[0:self.data_bits]
+
+    def _precompute(self):
+        parities = np.append(
+            np.logical_not(np.eye(self.parity_bits, dtype=int)),
+            np.ones((1, self.parity_bits), dtype=int),
+            axis=0)
+
+        self.G = np.append(np.eye(self.data_bits, dtype=int), parities, axis=1)
+        # print("G")
+        # print(self.G)
+
+        self.H = np.append(np.transpose(parities),
+                           np.eye(self.parity_bits, dtype=int), axis=1)
+        print("H")
+        print(self.H)
+
+        # Now recovering the data
+        self.POWER2 = np.array([2**i for i in range(self.parity_bits)],
+                               dtype=int)
+
+        self.correction_map = dict()
+        mle_map = dict()  # mle : Most Likely Explanation
+
+        # Compute all possible errors
+        for i in range(2**self.code_bits):
+            # See:
+            #  https://en.wikipedia.org/wiki/Decoding_methods#Syndrome_decoding
+            #  https://en.wikipedia.org/wiki/Hamming(7,4)#Error_correction
+
+            # The error that will be added to the correct data
+            # (for example during transmission)
+            error = bin_array(i, self.code_bits)
+
+            # Compute the effect of the error on the parity check
+            h_e = (self.H @ error) % 2
+            # print(f"{error} -> {h_e}")
+
+            # Evaluate the most likely explanation given the error
+            # To o that we compare the parity check value of that
+            # error with identical values given by other errors.
+            # The MLE is the error which has the smallest number of
+            # wrong bits (bit_in_error).
+
+            ndx = np.dot(self.POWER2, h_e)
+            bits_in_error = np.sum(error)
+            if ndx not in mle_map or bits_in_error < mle_map[ndx]:
+                mle_map[ndx] = bits_in_error
+
+                # Remember the most likely error associated to a given
+                # value of the parity check.
+                self.correction_map[ndx] = error
 
 
-parities = np.invert(np.eye(PARITY_BITS, dtype=np.bool))
-one_row = np.ones((1, PARITY_BITS))
-parities = np.append(parities, one_row, axis=0)
+if __name__ == "__main__":
+    hamming = HammingCode(4, 7)
 
-G = np.append(np.eye(DATA_BITS, dtype=np.bool), parities, axis=1)
-print(G)
+    # Check that all hamming codes decode properly
+    # (without error)
+    for i in range(2**4):
+        data = bin_array(i, 4)
+        check = hamming.decode(hamming.encode(data))
+        assert np.array_equal(check, data),  f"{data} != {check}"
 
-H = np.append(np.transpose(parities),
-              np.eye(PARITY_BITS, dtype=np.bool), axis=1)
-print("H")
-print(H)
+    # Introduce one bit of error in the data bits
+    for i in range(2**4):
+        for err in range(4):
+            data = bin_array(i, 4)
+            code = hamming.encode(data)
+            code[err] = 1 ^ code[err]
+            check = hamming.decode(code)
+            assert np.array_equal(check, data),\
+                f"{data} != {check}, error ndx={err}"
 
-x = np.array([1, 1, 1, 1])
-
-correct_code = (x @ G) % 2
-print()
-
-# Introducing an error
-code = correct_code.copy()
-code[0] = 0
-#code[1] = 0
-
-parity_check = (H @ code) % 2
-print(f"Correct code:{correct_code} Received code:{code} Parity:{parity_check}")
-
-
-POWER2 = np.array([2**i for i in range(PARITY_BITS)])
-
-correction_map = dict()
-mle_map = dict()
-
-for i in range(2**DATA_BITS):
-    # See https://en.wikipedia.org/wiki/Decoding_methods#Syndrome_decoding
-    error = bin_array(i, DATA_BITS)
-    h_e = (np.transpose(parities) @ error) % 2
-    print(f"{error} -> {h_e}")
-
-    ndx = np.dot(POWER2,h_e)
-    if ndx not in mle_map or np.sum(error) < mle_map[ndx]:
-        mle_map[ndx] = np.sum(error)
-        correction_map[ndx] = error
-
-print(correction_map[np.dot(POWER2,parity_check)])
+    # Introduce one bit of error in the parity bits
+    for i in range(2**4):
+        for err in range(4, 7):
+            data = bin_array(i, 4)
+            code = hamming.encode(data)
+            code[err] = 1 ^ code[err]
+            check = hamming.decode(code)
+            assert np.array_equal(check, data),\
+                f"{data} != {check}, error ndx={err}"
--- p2_channel.py
+++ p2_channel.py
@@ -1,26 +1,12 @@
-from utils import *
-#load_binary_text_sample(name="genome.txt",spaces=True)
-# load_text_sample()
-# print("--")
-
-# import numpy as np
-# f = np.genfromtxt("genome.txt",dtype='str')
-
-# with open("genome.txt","r") as fin:
-#     d = fin.read().replace("\n","")
-
-# print(d[:100])
-# exit()
-
 import random
-import math
 import numpy as np
-from utils import *
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
+import bitarray
 
-RATE, WAV = load_wav()
+from hamming import HammingCode, bin_array
 
+RATE, WAV = scipy.io.wavfile.read('sound.wav')
 # sound.wav: RIFF (little-endian) data, WAVE audio, Microsoft PCM, 8 bit, mono 11025 Hz
 
 # PCM : https://en.wikipedia.org/wiki/Pulse-code_modulation#Modulation
@@ -32,34 +18,31 @@ plt.plot(WAV)
 
 # QUESTION 16
 
-""" Note that in the data only 90 values out of the 256 possible
+""" Note that in the data, only 90 values out of the 256 possible
 ones are used. So we only need to be able to represent 90
 values. For that we need log_2 90 = 7 bits
-
 """
+
 unique_elements, counts_elements = np.unique(WAV, return_counts=True)
 
-print(len(unique_elements))
-print(math.ceil( math.log2(len(unique_elements))))
-
 plt.figure()
-plt.hist(WAV,bins=256)
-plt.show()
+plt.hist(WAV, bins=256)
 
 mapping = [None] * 256
-demapping = [128] * 256
+demapping = [2**7] * 256
 
 for i, e in enumerate(unique_elements):
-    mapping[e] = np.array([x == "1" for x in f"{i:7b}"])
+    mapping[e] = bin_array(i, 7)
     demapping[i] = e
 
 bin_encoded = np.zeros(7 * len(WAV), dtype=bool)
 for i in range(len(WAV)):
     bin_encoded[i*7:(i+1)*7] = mapping[WAV[i]]
 
-print(bin_encoded)
+print(f"Encoded sound is {len(bin_encoded)//8} bytes long, original was {len(WAV)}")
 
-decoded = np.zeros(len(WAV))
+# Make sure we can decode it
+decoded = np.zeros(len(WAV), dtype=np.uint8)
 for i in range(len(WAV)):
     decoded[i] = demapping[np.dot(bin_encoded[i*7:(i+1)*7], np.array([64,32,16,8,4,2,1]))]
 
@@ -68,17 +51,80 @@ assert decoded.all() == WAV.all()
 # QUESTION 17
 
 P = 0.01
-
-indices = random.sample(range(len(bin_encoded)), int(len(WAV)*P))
+indices = random.sample(range(len(bin_encoded)), int(len(bin_encoded)*P))
 bin_encoded_with_error = bin_encoded.copy()
 for i in indices:
     bin_encoded_with_error[i] = not bin_encoded_with_error[i]
 
-decoded = np.zeros(len(WAV),dtype=np.uint8)
+decoded = np.zeros(len(WAV), dtype=np.uint8)
 for i in range(len(WAV)):
     v = np.dot(bin_encoded_with_error[i*7:(i+1)*7], np.array([64,32,16,8,4,2,1]))
     decoded[i] = demapping[v]
 
-scipy.io.wavfile.write("decoded.wav", RATE, decoded)
+print("Wrote decoded_with_errors.wav")
+scipy.io.wavfile.write("decoded_with_errors.wav", RATE, decoded)
 
 # QUESTION 18
+
+DATA_BITS = 4
+CODE_BITS = 7
+hamming = HammingCode(DATA_BITS, CODE_BITS)
+
+# bits = bitarray.bitarray()
+# bits.frombytes(bytes(WAV.tolist()))
+
+bits = np.append(bin_encoded, [False] * (DATA_BITS - len(bin_encoded) % DATA_BITS))
+assert len(bits) % DATA_BITS == 0, "Padding is necesarry"
+
+hamming_bits = bitarray.bitarray((len(bits)//DATA_BITS)*CODE_BITS)
+
+print(f"Encoding {len(bits)} bits to {len(hamming_bits)} bits")
+j = 0
+for i in range(0, len(bits), DATA_BITS):
+    data = bits[i:i+DATA_BITS].tolist()
+    hcode = hamming.encode(data)
+    hamming_bits[j:j+CODE_BITS] = bitarray.bitarray(list(hcode))
+    j += CODE_BITS
+
+# QUESTION 19
+
+NB_ERRORS = int(len(hamming_bits)*P)
+print(f"Introducing {NB_ERRORS} errors with probability:{P}")
+indices = random.sample(range(len(hamming_bits)), NB_ERRORS)
+for i in indices:
+    hamming_bits[i] = hamming_bits[i] ^ 1
+
+print("Decoding and error-correction")
+decoded_bits = bitarray.bitarray()
+for i in range(0, len(hamming_bits), CODE_BITS):
+    hcode = hamming_bits[i:i+CODE_BITS]
+    code = hamming.decode(hcode.tolist())
+    decoded_bits.extend(bitarray.bitarray(list(code)))
+
+assert len(bits) == len(decoded_bits),\
+    f"The decoded data has not the same length as the original data! {len(decoded_bits)} != {len(bits)}"
+
+error = 0
+for i in range(len(bits)):
+    if bits[i] != decoded_bits[i]:
+        error += 1
+
+print(f"{error} errors on {len(bits)} => {error/len(bits):.3f}")
+
+decoded = np.zeros(len(WAV), dtype=np.uint8)
+for i in range(len(WAV)):
+    v = np.dot(decoded_bits[i*7:(i+1)*7].tolist(),
+               np.array([64, 32, 16, 8, 4, 2, 1]))
+    decoded[i] = demapping[v]
+
+scipy.io.wavfile.write("decoded_with_errors.wav", RATE, decoded)
+print("Wrote decoded_corrected.wav")
+
+print(np.min(decoded), np.max(decoded))
+plt.figure()
+plt.plot(decoded)
+plt.show()
+
+# QUESTION 20
+""" 20. How would you proceed to reduce the loss of information and/or
+to improve the communication rate? Justify. """
