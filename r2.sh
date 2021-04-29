283b3f979f4ebb7d742dae06427475428071b31c
Stéphane Champailler <schampailler@skynet.bewq>
2021-04-29 11:02:48 +0200

    fixxes

--- r2.sh
+++ r2.sh
@@ -6,10 +6,10 @@ rm -f -r release
 mkdir release
 cp p2_channel.py p2_huffman.py p2_LZ77.py p2_main.py p2_online_lz.py p2_utils.py hamming.py release
 cp genome.txt sound.wav release
-cp TechnoInfoCom.pdf release/${NAME}.pdf
 
-# pdflatex flight.tex
-# cp flight.pdf release/report.pdf
+pdflatex main.tex
+cp main.pdf release/${NAME}.pdf
+
 rm ${NAME}.zip
 zip -r ${NAME}.zip release --exclude \*.pyc --exclude \*.dot
 ls -l ${NAME}.zip
